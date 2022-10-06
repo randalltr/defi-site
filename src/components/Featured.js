08a03ab1e89b01ec74cf6223b64e8f25a3629738
Randall <108246257+randalltr@users.noreply.github.com>
2022-10-06 16:05:43 +0700

    added API data from CoinGecko

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -1,10 +1,24 @@
-import React from 'react'
-import BTC from '../assets/btc-img.png'
+import React, { useState, useEffect } from 'react'
 import {FiArrowUpRight, FiArrowDown} from 'react-icons/fi'
 import './Featured.css'
+import axios from 'axios'
 
 const Featured = () => {
+
+    const [data, setData] = useState(null)
+
+    const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=6&page=1&sparkline=false'
   
+    useEffect(() => {
+        axios.get(url).then((response) => {
+            setData(response.data)
+        }).catch((error) => {
+            console.log(error)
+        })
+    }, [])
+
+    if (!data) return null;
+
     return (
     <div className='featured'>
         <div className='container'>
@@ -18,16 +32,30 @@ const Featured = () => {
 
             {/* Right */}
             <div className='right'>
-                <div className='top'>
-                    <img src={BTC} alt='/' />
-                </div>
-                <div>
-                    <h5>Bitcoin</h5>
-                    <p>$20,000</p>
-                </div>
-                <span><FiArrowUpRight />2.5%</span>
-            </div>
 
+                <div className='card'>
+                    <div className='top'>
+                        <img src={data[0].image} alt='/' />
+                    </div>
+                    <div>
+                        <h5>{data[0].name}</h5>
+                        <p>${data[0].current_price.toLocaleString()}</p>
+                    </div>
+
+                    {data[0].price_change_percentage_24h > 0 ? (
+                        <span className='red'>
+                            <FiArrowDown />
+                            {data[0].price_change_percentage_24h.toFixed(2)}%
+                        </span>
+                    ) : (
+                        <span className='green'>
+                            <FiArrowUpRight />
+                            {data[0].price_change_percentage_24h.toFixed(2)}%
+                        </span>
+                    )}
+                </div>
+                
+            </div>
         </div>
     </div>
     )
